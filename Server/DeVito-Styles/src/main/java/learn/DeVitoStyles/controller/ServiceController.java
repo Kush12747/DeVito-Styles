2107fae123bbafd051bf34046209cbddaf906f1e
Kush Gandhi <kushgandhi2099@gmail.com>
2026-05-12 12:25:52 -0500

    service controller

--- Server/DeVito-Styles/src/main/java/learn/DeVitoStyles/controller/ServiceController.java
+++ Server/DeVito-Styles/src/main/java/learn/DeVitoStyles/controller/ServiceController.java
@@ -1,11 +1,15 @@
 package learn.DeVitoStyles.controller;
 
+import learn.DeVitoStyles.domain.Result;
+import learn.DeVitoStyles.domain.ResultType;
 import learn.DeVitoStyles.domain.ShopService;
+import learn.DeVitoStyles.models.Service;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/service")
@@ -17,5 +21,66 @@ public class ServiceController {
         this.service = service;
     }
 
+    @GetMapping
+    public ResponseEntity<Object> findAll() throws DataAccessException {
+        Result<List<Service>> result = service.findAll();
+        return ResponseEntity.ok(result.getpayload());
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> findById(@PathVariable int id) throws DataAccessException {
+        Result<Service> result = service.findById(id);
+
+        if (!result.isSuccess()) {
+            if (result.getResultType() == ResultType.NOT_FOUND) {
+                return new ResponseEntity<>(result.getMessages(), HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(result.getMessages(), HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.ok((result.getpayload()));
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody Service serviceObj) throws DataAccessException {
+        Result<Service> result = service.create(serviceObj);
+
+        if (!result.isSuccess()) {
+            return new ResponseEntity<>(result.getMessages(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(result.getpayload(), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable int id, @RequestBody Service serviceObj) throws DataAccessException {
+
+        if (id != serviceObj.getServiceId()) {
+            return new ResponseEntity<>(List.of("Path ID and body ID must match"), HttpStatus.CONFLICT);
+        }
+
+        Result<Service> result = service.update(serviceObj);
+
+        if (!result.isSuccess()) {
+            if (result.getResultType() == ResultType.NOT_FOUND) {
+                return new ResponseEntity<>(result.getMessages(), HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(result.getMessages(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(result.getpayload(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable int id) throws DataAccessException {
+        Result<Void> result = service.deleteById(id);
+
+        if (!result.isSuccess()) {
+            if (result.getResultType() == ResultType.NOT_FOUND) {
+                return new ResponseEntity<>(result.getMessages(), HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(result.getMessages(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
