b64d7cce2ccd49bfc73f7d0453d6936168baa564
Kush Gandhi <kushgandhi2099@gmail.com>
2026-05-12 15:43:25 -0500

    service testing

--- Server/DeVito-Styles/src/main/java/learn/DeVitoStyles/controller/BarberController.java
+++ Server/DeVito-Styles/src/main/java/learn/DeVitoStyles/controller/BarberController.java
@@ -1,4 +1,17 @@
 package learn.DeVitoStyles.controller;
 
+import learn.DeVitoStyles.domain.BarberService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@CrossOrigin
 public class BarberController {
+    private final BarberService service;
+
+    public BarberController(BarberService service) {
+        this.service = service;
+    }
 }
