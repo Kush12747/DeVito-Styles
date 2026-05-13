b6311666aa2597ac69c5d58dd233aa9caf4dbab0
Kush Gandhi <kushgandhi2099@gmail.com>
2026-05-13 08:33:57 -0500

    appointment mapper

--- Server/DeVito-Styles/src/main/java/learn/DeVitoStyles/data/mappers/AppointmentMapper.java
+++ Server/DeVito-Styles/src/main/java/learn/DeVitoStyles/data/mappers/AppointmentMapper.java
@@ -1,4 +1,24 @@
 package learn.DeVitoStyles.data.mappers;
 
-public class AppointmentMapper {
+import learn.DeVitoStyles.models.Appointment;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AppointmentMapper implements RowMapper<Appointment> {
+
+    @Override
+    public Appointment mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Appointment appointment = new Appointment();
+
+        appointment.setAppointmentId(rs.getInt("appointment_id"));
+        appointment.setUserId(rs.getInt("user_id"));
+        appointment.setBarberId(rs.getInt("barber_id"));
+        appointment.setServiceId(rs.getInt("service_id"));
+        appointment.setAppointmentDatetime(rs.getTimestamp("appointment_datetime").toLocalDateTime());
+        appointment.setStatus(rs.getString("status"));
+
+        return appointment;
+    }
 }
