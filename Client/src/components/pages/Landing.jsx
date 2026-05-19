9bf86d182c15c3fdb93c522310b866dda7734402
Kush Gandhi <kushgandhi2099@gmail.com>
2026-05-19 13:45:02 -0500

    landing css updated

--- Client/src/components/pages/Landing.jsx
+++ Client/src/components/pages/Landing.jsx
@@ -1,10 +1,33 @@
 import { Link } from "react-router-dom";
 import "../../styles/landing.css";
 import bg from "../../images/HomeBackground.png";
+import logo from "../../images/logo.png";
 
 function Home() {
     return (
         <div className="home-page">
+            
+            {/* floating particles */}
+            <div className="particles">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            <div className="home-card">
+                <div className="logo-wrapper">
+                    <img src={logo} alt="DeVito Styles" className="home-logo" />
+                </div>
+
             <div className="yellow-wrapper">
                 <div className="slogan-box">
                     <h1>Save your Style with DeVito Styles</h1>
@@ -20,6 +43,7 @@ function Home() {
                             </Link>
                         </div>
                     </div>
+            </div>
             </div>
         </div>
     )
