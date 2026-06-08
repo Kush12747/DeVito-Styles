43b5f8590fb4bb9c6034c55b990123fac62e52ec
Kush Gandhi <kushgandhi2099@gmail.com>
2026-06-08 08:18:14 -0500

    ChatMessage comments

--- Client/src/components/Chatbot/ChatMessage.jsx
+++ Client/src/components/Chatbot/ChatMessage.jsx
@@ -1,9 +1,50 @@
+/*
+ * ChatMessage Component
+ *
+ * Responsible for displaying
+ * a single message in the chat.
+ *
+ * Examples:
+ *
+ * User message:
+ * {
+ *   sender: "user",
+ *   text: "What are your hours?"
+ * }
+ *
+ * Bot message:
+ * {
+ *   sender: "bot",
+ *   text: "We are open 9AM-7PM."
+ * }
+ */
+
 function ChatMessage({ message }) {
+
     return (
-        <div 
+
+        /*
+         * Dynamic CSS classes.
+         *
+         * If sender is "user":
+         * className becomes:
+         * "message user"
+         *
+         * If sender is "bot":
+         * className becomes:
+         * "message bot"
+         *
+         * If sender is "typing":
+         * className becomes:
+         * "message typing"
+         */
+        <div
             className={`message ${message.sender}`}
         >
-            {message.text} 
+
+            {/* Actual text displayed */}
+            {message.text}
+
         </div>
     );
 }
