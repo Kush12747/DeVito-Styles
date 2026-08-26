48f8e4bc0f18c7a6b579a9ced89973b9ae2023fa
Kush Gandhi <kushgandhi2099@gmail.com>
2026-08-26 11:39:30 -0500

    Added empty cart page

--- Client/src/components/Checkout/Components/EmptyCart.jsx
+++ Client/src/components/Checkout/Components/EmptyCart.jsx
@@ -1,7 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import "../Style/EmptyCartStyle.css";
+
 function EmptyCart() {
+
+    const navigate = useNavigate();
+
     return (
-        <>
-        </>
+        <div className="emptyCart">
+            <div className="emptyCartIcon">
+                🛒
+            </div>
+
+            <h2>Your Cart is Empty</h2>
+
+            <p>You haven't added any products yet.
+                Browse our grooming collection and find something you like.
+            </p>
+
+            <button onClick={() => navigate("/products")}>
+                Browse Products
+            </button>
+        </div>
     );
 }
 
